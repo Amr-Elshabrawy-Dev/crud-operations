5a8a5c30f5727ae357d312502579f2c59f246f57
AmrElshabrawy <amrelshabrawy.dev@gmail.com>
2024-11-26 17:17:45 +0200

    Enhances geography chart with interactivity and styling
    Adds data categorization and interactive features to the geography chart component. Implements patterns for fill styles and configurations for tooltips on mouse events to improve user engagement. Updates VSCode launch configuration for quick testing.

--- src/components/charts/ChartGeographyCompo.jsx
+++ src/components/charts/ChartGeographyCompo.jsx
@@ -4,6 +4,13 @@ import { geoFeatures } from "../../data/mockGeoFeatures";
 import { useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 
+const dataFill = data.map((item) => {
+  return {
+    match: { id: item.id },
+    id: item.value > 250000 ? null : "dots",
+  };
+});
+
 const ChartGeographyCompo = ({ isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -53,14 +60,32 @@ const ChartGeographyCompo = ({ isDashboard = false }) => {
       features={geoFeatures.features}
       margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
       domain={[0, 1000000]}
+      colors="PuBuGn"
       unknownColor="#666666"
+      fillColor="properties.color"
       label="properties.name"
       valueFormat=".2s"
+      layers={["graticule", "features", "legends"]}
+      role="application"
+      ariaLabel="Choropleth map"
+      match={"id"}
+      value={"value"}
+      projectionType={"mercator"}
       projectionScale={isDashboard ? 40 : 150}
       projectionTranslation={!isDashboard ? [0.5, 0.5] : [0.49, 0.6]}
       projectionRotation={[0, 0, 0]}
+      enableGraticule={false}
+      graticuleLineWidth={1}
+      graticuleLineColor="#ffffff40"
       borderWidth={1.5}
       borderColor="#ffffff"
+      isInteractive={true}
+      onMouseEnter={() => {}}
+      onMouseMove={() => {}}
+      onMouseLeave={() => {}}
+      onClick={(place, x) => {
+        console.log("🚀 ~ place ~ :", place, x);
+      }}
       legends={
         !isDashboard
           ? [
@@ -90,6 +115,41 @@ const ChartGeographyCompo = ({ isDashboard = false }) => {
             ]
           : undefined
       }
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "inherit",
+          color: "#38bcb2",
+          size: 4,
+          padding: 2,
+          stagger: true,
+        },
+        {
+          id: "lines",
+          type: "patternLines",
+          background: "inherit",
+          color: "#eed312",
+          rotation: -45,
+          lineWidth: 6,
+          spacing: 10,
+        },
+        {
+          id: "gradient",
+          type: "linearGradient",
+          colors: [
+            {
+              offset: 0,
+              color: "#ffffff50",
+            },
+            {
+              offset: 100,
+              color: "inherit",
+            },
+          ],
+        },
+      ]}
+      fill={dataFill}
     />
   );
 };
